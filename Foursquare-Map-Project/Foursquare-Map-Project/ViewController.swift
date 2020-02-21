08e07233c40f171be10b99d1d25168c9f631c49f
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-02-21 15:22:03 -0500

    added xcode project

---
+++ Foursquare-Map-Project/Foursquare-Map-Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Foursquare-Map-Project
+//
+//  Created by Oscar Victoria Gonzalez  on 2/21/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
