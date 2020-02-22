82ec4cae2fa17ae214656a072a572f041b8391f4
oscar victoria <oscarvictoriagonzalez@gmail.com>
2020-02-22 00:52:16 -0500

    added tab bar controller

---
+++ Foursquare-Map-Project/Foursquare-Map-Project/Controllers/CollectionsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CollectionsViewController.swift
+//  Foursquare-Map-Project
+//
+//  Created by Oscar Victoria Gonzalez  on 2/22/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class CollectionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+        
+    }
+    
+
+ 
+
+}
---
+++ Foursquare-Map-Project/Foursquare-Map-Project/Controllers/TabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  TabBarController.swift
+//  Foursquare-Map-Project
+//
+//  Created by Oscar Victoria Gonzalez  on 2/22/20.
+//  Copyright © 2020 Oscar Victoria Gonzalez . All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+//    private lazy var viewController: ViewController = {
+//         let vc = ViewController(dataPersistence)
+//         vc.tabBarItem = UITabBarItem(title: "View", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+//         return vc
+//     }()
+    
+    private lazy var viewController: ViewController = {
+        let vc = ViewController()
+        vc.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+        return vc
+    }()
+    
+    private lazy var collectionsController: CollectionsViewController = {
+        let vc = CollectionsViewController()
+        vc.tabBarItem = UITabBarItem(title: "Collections", image: UIImage(systemName: "folder"), tag: 1)
+        return vc
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [viewController, collectionsController]
+       
+    }
+    
+
+ 
+
+}
